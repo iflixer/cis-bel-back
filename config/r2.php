2fa5e34727b163b605a0c528cb45d66bd720e22a
Drago <ivan.drago.ussr.true@gmail.com>
2026-03-03 09:05:20 +0100

    Refactor R2 token retrieval to use variable instead of direct env call

--- config/r2.php
+++ config/r2.php
@@ -1,7 +1,16 @@
 <?php
 
+$t = env('R2_TOKEN');
+$tf = env('R2_TOKEN_FILE');
+if ($tf!="") {
+    $t_ = file_get_contents($tf);
+    if (!empty($t_)) {
+        $t = $t_;
+    }
+} 
+
 return [
-    'token' => env('R2_TOKEN'),
+    'token' => $t,
     'endpoint' => env('R2_ENDPOINT'),
     'key_id' => env('R2_KEY_ID'),
 ];
